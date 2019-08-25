ba141d7b7cc4b04eb8c9eeba925fdbe855230f5b
Gabriel Moraes <gsan.moraes@gmail.com>
2019-08-25 06:40:04 -0300

    Tentando colocar csv

--- medplan/src/app/tab3/tab3.page.ts
+++ medplan/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  public pieChart: GoogleChartInterface;
 
   constructor() {}
+  
+  ionViewDidEnter() {
+    this.loadSimplePieChart();
+  }
 
+  loadSimplePieChart() {
+    this.pieChart = {
+      chartType: 'PieChart',
+      dataTable: [
+        ['Task', 'Hours per Day'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+      ],
+      //opt_firstRowIsData: true,
+      options: {
+        'title': 'Tasks',
+        height: 600,
+        width: '100%'
+      },
+    };
+  }
 }
